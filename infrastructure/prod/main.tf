7cfe1c8900d9cad3adf6ad3fb0c13a42a14b8607
lboesch <Lorenz Bsch>
2025-09-19 09:53:33 +0200

    fix: issuer

--- infrastructure/prod/main.tf
+++ infrastructure/prod/main.tf
@@ -3,7 +3,7 @@ locals {
   gcp_region     = "europe-west6"
   env            = "prod"
   zitadel_org_id = "245808142053571414"
-  zitadel_issuer = "clada.cas-fee-adv-ed1ide.zitadel.cloud"
+  zitadel_issuer = "cas-fee-adv-ed1ide.zitadel.cloud"
 }
 
 provider "google" {
